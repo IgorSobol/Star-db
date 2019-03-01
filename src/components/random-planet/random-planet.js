23c84661acc04dc9c29dc216a6377210bfb7ad71
Igor <sobol.dominus@gmail.com>
2019-03-01 13:47:45 +0300

    Add spinner css animation when data load in srver

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import SwapiService from '../../services/swapi-service';
-
+import Spinner from '../spinner';
 import './random-planet.css';
 
 
@@ -31,9 +31,11 @@ export default class RandomPlanet extends Component {
 
   render() {
     const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
+    
     return (
       <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
+        <Spinner />
+        {/* <img className="planet-image"
              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} 
              alt="" />
         <div>
@@ -52,7 +54,7 @@ export default class RandomPlanet extends Component {
               <span>{ diameter }</span>
             </li>
           </ul>
-        </div>
+        </div> */}
       </div>
 
     );
